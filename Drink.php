a729bb944624e818035ec1f0a5920eb947c89d5c
Juan <juan@Juans-MacBook-Pro.local>
2021-10-30 17:31:52 +0200

    Drink update

--- Drink.php
+++ Drink.php
@@ -8,42 +8,40 @@ class Drink extends Expendable {
     private $quantity;
     const MAX_QUANTITY = 250; //Int
 
-    protected function __construct($size, $numberUsers, $nameValue, $healthUp, $drinkUp, $quantity, MAX_QUANTITY) {
+    protected function __construct($size, $numberUsers, $nameValue, $healthUp, $drinkUp, $quantity) 
+    {
         parent::__construct($size, $numberUsers, $nameValue); //testear
         $this->healthUp = $healthUp;
         $this->drinkUp = $drinkUp;
         $this->quantity = $quantity;
-        $this->MAX_QUANTITY = MAX_QUANTITY;
     }
 
-    public function setHealthUp($healthUp) {
+    public function setHealthUp($healthUp) 
+    {
         $this->healthUp = $healthUp;
     }
     public function setDrinkUp($drinkUp) 
     {
         $this->drinkUp = $drinkUp;
     }
-    public function setQuantity($quantity) 
+    public function getHealthUp() 
     {
-        $this->quantity = MAX_QUANTITY; //duda valores q = MAX_Q
-    }
-    public function setMAX_QUANTITY($max) {
-        $this->MAX_QUANTITY = $max;
-    }
-    public function getHealthUp() {
         return $this->healthUp;
     }
-    public function getDrinkUp() {
+    public function getDrinkUp() 
+    {
         return $this->drinkUp;
     }
-    public function getQuantity() {
+    public function getQuantity() 
+    {
         return $this->quantity;
     }
-    public function MAX_QUANTITY() {
+    public function MAX_QUANTITY() 
+    {
         return $this->MAX_QUANTITY;
     }
-
-    public function __toString() {
+    public function __toString() 
+    {
         echo "$this->size, $this->numberUsers, $this->nameValue, $this->healthUp, $this->drinkUp, $this->quantity, $this->MAX_QUANTITY";
     }
 }
